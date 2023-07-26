ebaa0bf04e97be2de87beb5491560d006797faeb
Seth Vargas <shvargas01@gmail.com>
2023-07-26 13:52:02 -0500

    Finished work on user model

--- models/user.js
+++ models/user.js
@@ -2,105 +2,191 @@
 
 const { BCRYPT_WORK_FACTOR } = require("../config")
 const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
 const db = require("../db")
+const ExpressError = require("../expressError")
 
 /** User of the site. */
 
 class User {
 
-  constructor({ username, password, first_name, last_name, phone }) {
-    this.username = username;
-    this.password = password
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.phone = phone;
-  }
+    /** register new user -- returns
+     *    {username, password, first_name, last_name, phone}
+     */
 
-  /** register new user -- returns
-   *    {username, password, first_name, last_name, phone}
-   */
+    static async register({ username, password, first_name, last_name, phone }) {
+        try {
+            // TODO: move to /users/register
+            // if (!username || !password) {
+            //   throw new ExpressError("Username and password required", 400);
+            // }
 
-  static async register({ username, password, first_name, last_name, phone }) {
-    try {
-      // TODO: move to /register
-      // if (!username || !password) {
-      //   throw new ExpressError("Username and password required", 400);
-      // }
+            const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
 
-      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
-
-      const results = await db.query(`
-      INSERT INTO users (username, password, first_name, last_name, phone)
-      VALUES ($1, $2, $3, $4, $5)
-      RETURNING username, password, first_name, last_name, phone`,
-        [username, hashedPassword, first_name, last_name, phone])
-      return new User(results.rows[0])
-    } catch (error) {
-      console.log(error)
+            const result = await db.query(`
+                INSERT INTO users (
+                    username, 
+                    password, 
+                    first_name, 
+                    last_name, 
+                    phone,
+                    last_login_at
+                )
+                VALUES (
+                    $1, 
+                    $2, 
+                    $3, 
+                    $4, 
+                    $5,
+                    $6
+                )
+                RETURNING 
+                    username, 
+                    password,
+                    first_name,
+                    last_name, 
+                    phone
+                `,
+                [username, hashedPassword, first_name, last_name, phone, new Date()]
+            )
+            return result.rows[0]
+        } catch (error) {
+            console.log(error)
+            process.exit(1)
+        }
     }
-  }
 
-  /** Authenticate: is this username/password valid? Returns boolean. */
+    /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) {}
+    static async authenticate(username, password) {
+        try {
+            const { rows } = await db.query(`
+      SELECT password FROM users
+      WHERE username = $1`, [username])
 
-  /** Update last_login_at for user */
+            const found = rows[0]
 
-  static async updateLoginTimestamp(username) { }
+            if (found !== undefined) {
+                const passwordIsValid = await bcrypt.compare(password, found.password)
 
-  /** All: basic info on all users:
-   * [{username, first_name, last_name, phone}, ...] */
+                return passwordIsValid
+            }
+            return false
+        } catch (error) {
+            console.log(error)
+            process.exit(1)
+        }
+    }
 
-  static async all() {
-    const results = await db.query(`
+    /** Update last_login_at for user */
+
+    static async updateLoginTimestamp(username) {
+        try {
+            await db.query(`
+                    UPDATE users 
+                    SET last_login_at = CURRENT_TIMESTAMP 
+                    WHERE username = $1
+                `,
+                [username]
+            )
+        } catch (error) {
+            console.log(error)
+            process.exit(1)
+        }
+    }
+
+    /** All: basic info on all users:
+     * [{username, first_name, last_name, phone}, ...] */
+
+    static async all() {
+        const { rows } = await db.query(`
       SELECT username, first_name, last_name, phone
       FROM users`
-    )
-    return results.rows
-  }
-
-  /** Get: get user by username
-   *
-   * returns {username, first_name, last_name, phone, join_at, last_login_at } */
-
-  static async get(username) {
-    const results = await db.query(`
-      SELECT username, first_name, last_name, phone, join_at, last_login_at 
-      FROM users WHERE username = $1`,
-      [username]
-    )
-
-    const user = results.rows[0]
-
-    if (user === undefined) {
-      const err = new Error(`User not found: ${username}`)
-      err.status = 404
-      throw err
+        )
+        return rows
     }
 
-    return user
-  }
+    /** Get: get user by username
+     *
+     * returns {username, first_name, last_name, phone, join_at, last_login_at } */
 
-  /** Return messages from this user.
-   *
-   * [{id, to_user, body, sent_at, read_at}]
-   *
-   * where to_user is
-   *   {username, first_name, last_name, phone}
-   */
+    static async get(username) {
+        const { rows } = await db.query(`
+            SELECT 
+                username,
+                first_name, 
+                last_name, 
+                phone, 
+                join_at, 
+                last_login_at 
+            FROM users 
+            WHERE username = $1`,
+            [username]
+        )
 
-  static async messagesFrom(username) { }
+        const user = rows[0]
 
-  /** Return messages to this user.
-   *
-   * [{id, from_user, body, sent_at, read_at}]
-   *
-   * where from_user is
-   *   {username, first_name, last_name, phone}
-   */
+        if (!user) {
+            throw new ExpressError(`No such user: ${username}`, 404);
+        }
 
-  static async messagesTo(username) { }
+        return user
+    }
+
+    /** Return messages from this user.
+     *
+     * [{id, to_user, body, sent_at, read_at}]
+     *
+     * where to_user is
+     *   {username, first_name, last_name, phone}
+     */
+
+    static async messagesFrom(username) {
+        const request = await db.query(`
+            SELECT 
+                id, 
+                to_username, 
+                body, 
+                sent_at,
+                read_at
+            FROM messages
+            WHERE from_username = $1
+        `, [username])
+
+        const messages = []
+        for (let row of request.rows) {
+            const user = await User.get(row.to_username)
+            const { first_name, last_name, phone, username } = user
+            const { id, body, sent_at, read_at } = row
+            messages.push({ id, to_user: { first_name, last_name, phone, username }, body, sent_at, read_at })
+        }
+        return messages
+    }
+
+    /** Return messages to this user.
+     *
+     * [{id, from_user, body, sent_at, read_at}]
+     *
+     * where from_user is
+     *   {username, first_name, last_name, phone}
+     */
+
+    static async messagesTo(username) {
+        const request = await db.query(`
+          SELECT id, from_username, body, sent_at, read_at
+          FROM messages
+          WHERE to_username = $1
+          `,
+            [username])
+        const messages = []
+        for (let row of request.rows) {
+            const user = await User.get(row.from_username)
+            const { first_name, last_name, phone, username } = user
+            const { id, body, sent_at, read_at } = row
+            messages.push({ id, from_user: { first_name, last_name, phone, username }, body, sent_at, read_at })
+            
+        }
+        return messages
+    }
 }
 
 
